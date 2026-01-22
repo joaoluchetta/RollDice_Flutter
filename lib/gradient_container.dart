93a14b15a9e607924ace73fd677c55d2f7d641ae
joaoluchetta <joaoluchetta.dev@gmail.com>
2026-01-22 14:12:46 -0300

    component color

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -2,25 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:roll_dice/styled_text.dart';
 
 class GradiantContainer extends StatelessWidget {
-  const GradiantContainer({super.key});
+  const GradiantContainer({super.key, required this.colors} /*this.firstColor, this.secondColor,*/);
+
+  final List<Color> colors;
+  //final Color firstColor;
+  //final Color secondColor;
 
   @override
   Widget build(context) {
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: const [
-            Color.fromARGB(255, 0, 4, 255),
-            Color.fromARGB(255, 59, 122, 218),
-          ],
+          colors: colors,
           begin: Alignment.topLeft,
           end: Alignment.bottomRight,
         ),
       ),
-      child: const Center(
-        child: StyledText('Hello World!')
-      ),
+      child: const Center(child: StyledText('Hello World!')),
     );
   }
 }
-
--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,12 @@ import 'package:roll_dice/gradient_container.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(body: GradiantContainer()),
+      home: Scaffold(
+        body: GradiantContainer(colors:[
+          Color.fromARGB(255, 0, 4, 255),
+          Color.fromARGB(255, 59, 122, 218)]
+        ),
+      ),
     ),
   );
 }
